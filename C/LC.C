41ea9648f65bca53c75f436144b6dc33ed0a9198
Caleb O'Neal <caleboneal07@gmail.com>
2024-12-04 09:04:48 -0500

    Allow unlimited file inputs to LC.C

--- C/LC.C
+++ C/LC.C
@@ -7,27 +7,28 @@ char *argv[];
     int c, count;
     count = 0;
     FILE *input;
-    switch (argc) {
-        /* Command Line Input */
-        case 1:
-            while ((c = getchar()) != EOF   )
-                if (c == '\n')
-                    ++count;
-            printf("\nThere are %d newlines.\n", count);
-            return;
-        /* File Input */
-        case 2:
-            input = fopen(argv[1], "r");
+
+    /* Command Line Input */
+    if (argc == 1) {
+        while ((c = getchar()) != EOF   )
+            if (c == '\n')
+                ++count;
+        printf("\nYou entered %d newlines\n", count);
+        return;
+
+    /* File Input */
+    } else {
+        int i;
+        for (i = 1; i < argc; i++) {
+            count = 0;
+            input = fopen(argv[i], "r");
 
             while ((c = fgetc(input)) != EOF)
                 if (c == '\n')
                     ++count;
-            printf("There are %d newlines in %s.\n", count, argv[1]);
-            return;
-        /* Too many args */
-        default:
-            printf("This command accepts either a single file or user input");
-            return;
-            
+            printf("There are %d newlines in %s\n", count, argv[i]);
+            fclose(input);
+        }
+        return;
     }
 }
