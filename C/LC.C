56b90ddcb2b0e8ff915775e42f769b69ed4cdbdd
Caleb O'Neal <caleboneal07@gmail.com>
2024-12-04 08:56:28 -0500

    update line counter to accept input from standard in or a file

--- C/LC.C
+++ C/LC.C
@@ -1,12 +1,33 @@
 #include <stdio.h>
 
-main()
+main(argc, argv)
+int argc;
+char *argv[];
 {
-    int c, nl;
+    int c, count;
+    count = 0;
+    FILE *input;
+    switch (argc) {
+        /* Command Line Input */
+        case 1:
+            while ((c = getchar()) != EOF   )
+                if (c == '\n')
+                    ++count;
+            printf("\nThere are %d newlines.\n", count);
+            return;
+        /* File Input */
+        case 2:
+            input = fopen(argv[1], "r");
 
-    nl = 0;
-    while ((c = getchar()) != EOF   )
-        if (c == '\n')
-            ++nl;
-    printf("%d\n", nl);
+            while ((c = fgetc(input)) != EOF)
+                if (c == '\n')
+                    ++count;
+            printf("There are %d newlines in %s.\n", count, argv[1]);
+            return;
+        /* Too many args */
+        default:
+            printf("This command accepts either a single file or user input");
+            return;
+            
+    }
 }
